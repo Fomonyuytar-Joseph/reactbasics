9ea66d739f4571bb174ac9fb238978417fbdb68e
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-06-20 12:37:49 +0100

    init

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { faker } from '@faker-js/faker';
 
-function CommentDetail({author}) {
+function CommentDetail({author ,timeAgo}) {
   return (
     <div className="comment ">
     <a href="/" className="avatar">
@@ -13,7 +13,7 @@ function CommentDetail({author}) {
         </a>
         <div className="metadata">
 
-            <span className="date"> Today at 6:00pm</span>
+            <span className="date">{timeAgo}</span>
         </div>
         <div className="text"> Nice blog post</div>
     </div>
--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const App =()=>{
     return (
         <div className=" ui container comments">
            
-            <CommentDetail author="Sam"/>
-            <CommentDetail author="Elliot"/>
-            <CommentDetail author="Joseph"/>
+            <CommentDetail author="Sam" timeAgo="Today at 4:12" commentText="nice pic"/>
+            <CommentDetail author="Elliot" timeAgo="Today at 3:12" commentText="Nice blog"/>
+            <CommentDetail author="Joseph" timeAgo="Today at 1:12" commentText="Thanks for sharing"/>
 
         </div>
     )
