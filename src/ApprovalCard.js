657ce4cd172d6291492f46a23bf6d3ff5bda3be3
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-06-21 13:01:43 +0100

    init

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 function ApprovalCard() {
   return (
-    <div>ApprovalCard</div>
+    <div class="ui cards">
+       <div class="content">Are you sure?</div>
+       <div class=" extra content">
+        <div className='ui two buttons'>
+          
+        <div class="ui basic green button">Approve</div>
+        <div class="ui basic red button">Decline</div>
+          
+           </div>
+       </div>
+      </div>
+     
+      
   )
 }
 
