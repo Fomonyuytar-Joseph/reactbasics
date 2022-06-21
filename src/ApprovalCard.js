4f9dbe492e01bd261b04ff3f29bbe04c1a603678
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-06-21 13:56:06 +0100

    init

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-function ApprovalCard() {
+function ApprovalCard(props) {
+  console.log(props.children)
   return (
-    <div class="ui cards">
-       <div class="content">Are you sure?</div>
+    <div class="ui card">
+       <div class="content">{props.children}</div>
        <div class=" extra content">
         <div className='ui two buttons'>
-          
+
         <div class="ui basic green button">Approve</div>
-        <div class="ui basic red button">Decline</div>
+        <div class="ui basic red button">Reject</div>
           
            </div>
        </div>
--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
-const App =()=>{
-    return (
-        <div className=" ui container comments">
-           
-            <CommentDetail author="Sam" timeAgo="Today at 4:12" commentText="nice pic" profilepic={faker.image.avatar()}/>
-            <CommentDetail author="Elliot" timeAgo="Today at 3:12" commentText="Nice blog" profilepic={faker.image.avatar()}/>
-            <CommentDetail author="Joseph" timeAgo="Today at 1:12" commentText="Thanks for sharing" profilepic={faker.image.avatar()}/>
+const App = () => {
+  return (
+    <div className=" ui container comments">
 
-        </div>
-    )
-}
+        <ApprovalCard>
+            <h4>Warning</h4>
+            are you sure you want to do this?
+        </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:12"
+          commentText="nice pic"
+          profilepic={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Elliot"
+          timeAgo="Today at 3:12"
+          commentText="Nice blog"
+          profilepic={faker.image.avatar()}
+        />
+      </ApprovalCard>
 
-ReactDOM.render(<App/>,document.querySelector("#root"));
+      <ApprovalCard>
+        <CommentDetail
+          author="Joseph"
+          timeAgo="Today at 1:12"
+          commentText="Thanks for sharing"
+          profilepic={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
